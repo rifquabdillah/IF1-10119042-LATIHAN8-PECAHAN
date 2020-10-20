704760ea3193740a61f3d115f050e13bdbcc0363
rifquabdillah <72897695+rifquabdillah@users.noreply.github.com>
2020-10-20 13:43:00 +0700

    Add files via upload

---
+++ IF110119042Latihan8pecahan.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanpbo;
+
+/**
+ *
+ * @author Stks
+ */
+public class IF110119042Latihan8pecahan {
+    
+ public static void main(String[] args){
+ float f;
+ double d;
+ f = 1234567890.123456789F;
+ d = 1_234_567_890.123456789;
+ System.out.println("Float :  " + f);
+ System.out.println("Double: " +d);
+ }
+
+    
+}
